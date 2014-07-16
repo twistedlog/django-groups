cee08cde0ea87aecdbe2a71d7ee026f2e37f9aca
sahil.chug <sahil.chug@fireeye.com>
2014-07-16 21:44:54 +0530

    changing DB settings

--- groups/groups/settings.py
+++ groups/groups/settings.py
@@ -57,8 +57,9 @@ WSGI_APPLICATION = 'groups.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'groups',
+        'USER': 'root',
     }
 }
 
